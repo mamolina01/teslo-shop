2535534f3ff4b96b6fe14aaf28fbe1280322e5a5
MatiasMolinaDev <matiasnmolina1@gmail.com>
2024-05-05 13:52:39 -0300

    implement section-21

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -1,9 +1,12 @@
 import { initialData } from './seed'
 import prisma from '../lib/prisma'
+import { countries } from './seed-countries'
 
 async function main() {
   // 1. Remove previous registers
+  await prisma.userAddress.deleteMany()
   await prisma.user.deleteMany()
+  await prisma.country.deleteMany()
   await prisma.productImage.deleteMany()
   await prisma.product.deleteMany()
   await prisma.category.deleteMany()
@@ -12,6 +15,10 @@ async function main() {
 
   await prisma.user.createMany({
     data: users
+  })
+
+  await prisma.country.createMany({
+    data: countries
   })
 
   const categoriesData = categories.map(category => ({
